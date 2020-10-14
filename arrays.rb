befcbc2768ba90116481070dfae97c83399a0674
Ben Bryant <brbryant2639@gmail.com>
2020-10-14 13:18:19 -0500

    Finished arrays in ruby practice.

--- arrays.rb
+++ arrays.rb
@@ -8,13 +8,28 @@ next_name1 =  "Denise"
 next_name2 =  "Jeremy"
 next_name3 =  "John"
 
-# .push to add to end of array
+# .push(value) to add to end of array
 names.push(next_name1)
 
-# .unshift to add to start of array
+# .unshift(value) to add to start of array
 names.unshift(next_name2)
 
 # .insert(index, value) to add to specific point in an array
 names.insert(3, next_name3)
+
+# .pop to remove last value in array
+names.pop
+
+# .shift to remove first item in array
+names.shift
+
+# .delete_at(index) to remove value at a certain index in array
+names.delete_at(3)
+
+# .delete(value) to remove a value from the array **This will remove all instances of this value in the array
+names.delete("Ben")
+
+# Arrays can be values in arrays
+names.push([1, 2, 3, 4])
 
 puts names
